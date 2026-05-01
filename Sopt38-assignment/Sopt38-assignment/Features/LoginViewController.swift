4023c5aafc1f63819afd2e0f6a3332792b3cb9ca
hemssy <hemsyeons@gmail.com>
2026-05-01 14:38:06 +0900

    [Design] #1 - 로그인뷰 넥스트버튼 인셋값 수정

--- Sopt38-assignment/Sopt38-assignment/Features/LoginViewController.swift
+++ Sopt38-assignment/Sopt38-assignment/Features/LoginViewController.swift
@@ -129,7 +129,7 @@ final class LoginViewController: UIViewController {
         
         nextButton.snp.makeConstraints {
             $0.horizontalEdges.equalToSuperview().inset(22)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(32)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(13)
             $0.height.equalTo(56)
         }
     }
