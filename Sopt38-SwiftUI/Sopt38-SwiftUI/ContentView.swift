a40156f09ccd7e8cf153de430715615e35cbc8a3
seoyeon <hemsyeons@gmail.com>
2026-05-29 02:16:14 +0900

    [Chore] #5 - SwiftUI 프로젝트 추가

---
+++ Sopt38-SwiftUI/Sopt38-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sopt38-SwiftUI
+//
+//  Created by 신서연 on 5/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
